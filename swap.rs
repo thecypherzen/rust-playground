7e08ced8308dfc61a1051738c5278d4386902af6
William Inyam <sw.inyam@outlook.com>
2025-09-25 13:33:32 +0100

    :star: feat: add generic variable swap func

--- swap.rs
+++ swap.rs
@@ -1,6 +1,7 @@
 #[allow(dead_code)]
 
 use std::fmt::*;
+use std::ops::BitXor;
 
 struct Pair(i32, i32);
 
@@ -19,21 +20,33 @@ impl Pair {
 }
 
 // Implement a generic swapping function
-fn swap_numbers(i: &mut i32, j: &mut i32) {
+fn swap_integers<T>(i: &mut T, j: &mut T)
+where T: BitXor<Output=T> + Copy {
 	*i = *i ^ *j;
 	*j = *i ^ *j;
 	*i = *i ^ *j;
+}
+
+fn swap<T>(i: &mut T, j: &mut T) {
+	std::mem::swap(i, j)
 }
 
 fn main() {
 	let pair = Pair(3, 420);
 	println!("--- Swapping a Tuple Struct ---");
 	println!("Original:{} Swapped:{}", pair, pair.swap());
-	println!("--- Swapping Two Variables ---");
+	println!("\n--- Swapping Two Integers ---");
 
-	let mut a = 13;
-	let mut b = -10;
-	println!("(before) => a: {} b: {}", a, b);
-	swap_numbers(&mut a, &mut b);
-	println!("(after) => a: {} b: {}", a, b);
+	let mut a = 14;
+	let mut b = -12;
+	println!("(before) => int1: {} int2: {}", a, b);
+	swap_integers(&mut a, &mut b);
+	println!("(after) => int1: {} int2: {}", a, b);
+	println!("\n--- Swapping Two Floats ---");
+
+	let mut c = 14.33;
+	let mut d = 1.24;
+	println!("(before) => f1: {} f2: {}", c, d);
+	swap(&mut c, &mut d);
+	println!("(after) => f1: {} f2: {}", c, d);
 }
