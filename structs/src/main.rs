f6b525e0235b45705a55da6bb4388af56bf02011
William Inyam <sw.inyam@outlook.com>
2025-09-29 07:40:21 +0100

    :star: up: complete generic macro

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,24 +1,37 @@
 // a struct with x & y props
-//struct Point {
-//	x: i32, y: i32
-//}
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Point {
+	x: f64, y: f64
+}
+
+impl Point {
+	fn new<X: Into<f64>, Y: Into<f64>>(x: X, y: Y) -> Self {
+		Point { x: x.into(), y: y.into()}
+	}
+}
 
 macro_rules! point {
-	() => {};
+	() => {Vec::<Point>::new()};
 	(
 		{x: $x:expr, y: $y:expr}$(,$($rest:tt)*)?
-	) => {
-		println!("x: {}, y: {}", $x, $y);
-		$( point!($($rest)*) )*
-	};
+	) => {{
+		let mut vect = Vec::<Point>::new();
+		vect.push(Point::new($x, $y));
+		vect.extend(point!($($($rest)*)?));
+		vect
+	}};
 	(
 		{y: $y:expr, x: $x:expr}$(,$($rest:tt)*)?
-	) => {
-		println!("x: {}, y: {}", $x, $y);
-		$( point!($($rest)*) )*
-	};
+	) => {{
+		let mut v2 = Vec::<Point>::new();
+		v2.push(Point::new($x, $y));
+		v2.extend(point!($($($rest)*)?));
+		v2
+	}};
 }
 
 fn main() {
-		point!({x: 2.2, y:2.0}, {y: 14, x: 13}, {x: 19.0, y: 22.9}, {x: 123.92, y: 55.550});
+		let r = point!({x: 22, y:2}, {y: 1.2f64, x: 13.09f64}, {x: 19.0f32, y: 22f32}, {x: 123.92f64, y: 55.55f32});
+		println!("Points::{:?}", r);
 }
