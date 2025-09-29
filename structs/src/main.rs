ae03399779f79a53a4b07e5957dcac47bdbd908a
William Inyam <sw.inyam@outlook.com>
2025-09-29 14:39:39 +0100

    :star: feat: solve distance-from-center task

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,21 +1,35 @@
 // a struct with x & y props
 #![allow(dead_code)]
 
+/**
+ * to_precision: a function that rounds a float to specified
+ * precision
+ */
 fn to_precision<T: Into<f64>>(n:T, p: u32) -> f64 {
 	let factor = 10i32.pow(p) as f64;
 	(n.into() * factor).round() / factor
 }
 
+// define Point struct
 #[derive(Debug)]
 struct Point {
 	x: f64, y: f64
 }
 
+// implement a Point constructor
 impl Point {
 	fn new<X: Into<f64>, Y: Into<f64>>(x: X, y: Y) -> Self {
 		Point { x: x.into(), y: y.into()}
 	}
+	/**
+	 * Calculates the distance from origin of a point
+	 */
+	fn distance_from_origin(&self) -> f64 {
+		to_precision((self.x.powi(2)+ self.y.powi(2)).sqrt(), 2)
+	}
 }
+
+
 
 // Point Generation Macro
 macro_rules! point {
@@ -35,6 +49,8 @@ macro_rules! point {
 }
 
 fn main() {
-		let r = point!({x: 22, y:2}, {y: 1.2f64, x: 13.09f64}, {x: 19.0f32, y: 22f32}, {x: 123.92f64, y: 55.55f32});
-		println!("Points::{:?}", r);
+		let points = point!({x: 22, y:2}, {y: 1.2f64, x: 13.09f64}, {x: 19.0f32, y: 22f32}, {x: 123.92f64, y: 55.55f32});
+		let [first, rest @..] = points.as_slice() else { todo!() };
+		println!("Point1: {:?}\nRest: {:?}", first, rest);
+		println!("Point1 distance from origin: {}", first.distance_from_origin());
 }
