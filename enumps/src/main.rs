19f7a8c72c16f9bdd8756e02b3bd8e7ce3ed0708
William Inyam <sw.inyam@outlook.com>
2025-09-30 22:34:47 +0100

    :star2: implement linked list - complete enums tasks

--- enumps/src/main.rs
+++ enumps/src/main.rs
@@ -2,10 +2,20 @@
 
 use crate::Direction::*;
 use std::fmt::{Display, Formatter, Result};
+use num_traits::{FromPrimitive};
+use std::cmp::PartialEq;
+use std::ops::Div;
 
 // Direction enum
 enum Direction {
 	North, South, East, West, Move(i32, i32)
+}
+
+#[derive(Debug)]
+// ResultLike enum
+enum ResultLike<T, E> {
+	Ok(T),
+	Err(E)
 }
 
 // Option like enum
@@ -14,6 +24,11 @@ enum OptionLike<T> {
 	None
 }
 
+enum Errors {
+	DivisionByZero
+}
+
+// display for OptionLike
 impl Display for Direction {
 	fn fmt(&self, f: &mut Formatter) -> Result {
 		write!(f, "{}", match *self {
@@ -36,6 +51,60 @@ fn empty_vector<T: std::fmt::Debug>(vect: &mut Vec<T>) {
 	}
 }
 
+fn divide<T:>(x:T, y:T) -> ResultLike<f64, Errors>
+where T: PartialEq<T> + FromPrimitive + Into<f64> + Div {
+	if y == T::from_i32(0).unwrap() {
+		ResultLike::Err(Errors::DivisionByZero)
+	} else {
+		let res = x.into() / y.into();
+		ResultLike::Ok(res)
+	}
+}
+
+/**
+ * Linked List
+ */
+#[derive(Debug)]
+enum List<T: Display + Copy> {
+	Node(T, Box<List<T>>),
+	Nil
+}
+
+impl<T: Display + Copy> List<T> {
+	fn new() -> List<T> {
+		List::Nil
+	}
+
+	fn prepend(self, elem: T) -> List<T> {
+		List::Node(elem, Box::new(self))
+	}
+
+	fn stringify(&self) -> String {
+		match self {
+			List::Node(h, t) => format!("{}{}{}", t.stringify(), if t.len() > 0 {", "} else {""}, h),
+			List::Nil => String::new()
+		}
+	}
+
+	fn len(&self) -> u32 {
+		match self {
+			List::Node(_, rest) => 1u32 + rest.len(),
+			List::Nil => 0u32
+		}
+	}
+
+	fn print(&self) {
+		println!("{}", self);
+	}
+}
+
+
+impl<T: Display + Copy> Display for List<T> {
+	fn fmt(&self, f: &mut Formatter) -> Result {
+			write!(f, "[{}]", self.stringify())
+	}
+}
+
 fn main() {
     let dir = North;
 		let mv = Move(24, 333);
@@ -54,4 +123,26 @@ fn main() {
 
 	let mut v1 = vec![10, 2];
 	empty_vector(&mut v1);
+	let nums = ((3, 4), (9.0, -3.0), (13, 0));
+	if let ResultLike::Ok(res2) = divide((nums.0).0, (nums.0).1) {
+		println!("{}/{} = {}", (nums.0).0, (nums.0).1, res2);
+	} else {
+		println!("{}/{} = Error", (nums.0).0, (nums.0).1);
+	}
+
+	let div2 =  divide((nums.1).0, (nums.1).1);
+	match div2 {
+		ResultLike::Ok(res2) => println!("{}/{} = {}", (nums.1).0, (nums.1).1, res2),
+		_ => println!("{}/{} = Error", (nums.1).0, (nums.1).1)
+	}
+	let div3 = divide((nums.2).0, (nums.2).1);
+	match div3 {
+		ResultLike::Ok(res3) => println!("{}/{} = {}", (nums.2).0, (nums.2).1, res3),
+		ResultLike::Err(Errors::DivisionByZero) => println!("{}/{} = Error", (nums.2).0, (nums.2).1)
+	}
+
+	println!("\n------ LINKED LIST --------");
+	let list_1 = List::new().prepend(2.0).prepend(2.5).prepend(3.0);
+	println!("List: {}", list_1);
+	list_1.print();
 }
