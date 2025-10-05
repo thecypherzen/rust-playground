f13684ead99d60c7e7b44be9f760f74a3c1989a9
William Inyam <sw.inyam@outlook.com>
2025-10-05 15:42:10 +0100

    :up::truck: update: resolve import errors + setup base App component

--- texios/client/src/App.tsx
+++ texios/client/src/App.tsx
@@ -1,8 +1,26 @@
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "./components/ui/card";
+
 function App() {
   return (
-    <>
-      <p>Welcome</p>
-    </>
+    <Card>
+      <CardHeader>
+        <CardDescription className="scr-only"> Upload a file</CardDescription>
+      </CardHeader>
+      <CardContent></CardContent>
+      <CardFooter>
+        <Button variant="ghost" className="cursor-pointer">
+          {" "}
+          Upload{" "}
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
 
