58098c1a07af4274df123c0077064041165dc50b
William Inyam <sw.inyam@outlook.com>
2025-10-08 13:09:13 +0100

    :up: update: read file for some supported files

--- texios/client/src/App.tsx
+++ texios/client/src/App.tsx
@@ -1,5 +1,5 @@
 //import { Button } from "./components/ui/button";
-import { CloudUpload, File, X } from "lucide-react";
+import { CloudUpload, EllipsisVertical, File, X } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -9,11 +9,20 @@ import {
   CardHeader,
   CardTitle,
 } from "./components/ui/card";
-import { Button } from "./components/ui/button";
-import { Progress } from "./components/ui/progress";
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 import { useEffect, useState } from "react";
+import { FileSelect } from "./components/FileSelect";
 
 function App() {
+  const [analysisResult, setAnalysisResult] = useState<TextAnalysisRes | null>(
+    null
+  );
+
+  useEffect(() => {
+    console.log("analysis result:", analysisResult);
+  }, [analysisResult]);
+
   return (
     <div className="m-auto flex h-screen flex-col items-center justify-center p-3">
       <Card className="min-h-3/4 w-full md:w-2/3 lg:w-1/2 shadow-none gap-2">
@@ -29,47 +38,20 @@ function App() {
           </div>
         </CardHeader>
         <CardContent className="flex flex-col gap-4 border-0 border-neutral-300 flex-1 pt-5">
-          {/* Upload Action */}
-          <div className="flex flex-col gap-3 justify-center items-center border-1 border-dashed border-gray-300 rounded-lg p-6 flex-grow flex-3/4">
-            <CloudUpload className="size-12 text-neutral-300" />
-            <div className="flex flex-col gap-1 justify-center items-center">
-              <p>Choose file or drag and drop it here</p>
-              <p className="text-gray-400 text-xs">
-                TXT, PDF, MD, DOC DOCX up to&nbsp;<span>50mb</span>
-              </p>
-            </div>
-            <Button variant="outline" size="sm" className="mt-3 cursor-pointer">
-              Choose File
-            </Button>
-          </div>
+          <FileSelect
+            analyseText={(t: string) => {
+              textAnalyse(t)
+                .then((res) => {
+                  setAnalysisResult(res);
+                })
+                .catch((err: any) => {
+                  console.error(err);
+                });
+            }}
+          />
         </CardContent>
         {/* Uploaded Content */}
-        <CardFooter className="flex flex-col gap-1">
-          {/* Uploading State */}
-          <div className="flex flex-col gap-2 justify-center items-center bg-neutral-100 rounded-lg p-8 w-full">
-            {/* header */}
-            <div className="flex w-full justify-between items-start mb-2 border-1 border-neutral-500">
-              <div className="flex gap-3 items-center">
-                {/* Icon */}
-                <div className="size-10 rounded-md bg-white shadow-md shadow-gray-200 flex flex-col items-center justify-center p-2">
-                  <File />
-                </div>
-                {/* file info */}
-                <div className="flex flex-col justify-center">
-                  <p className="font-medium text-sm">Filename.ext</p>
-                  <p className="font-normal text-xs text-gray-400">3 MB</p>
-                </div>
-              </div>
-              {/* close Btn */}
-              <div className="p-1 rounded-full hover:border-1 hover:border-gray-500 cursor-pointer transition-all duration-100 ease-in-out">
-                <X size={16} />
-              </div>
-            </div>
-            <ProgressBar />
-          </div>
-          {/* Uploaded State */}
-          <div className="flex flex-col gap-2 justify-center items-center bg-neutral-100 rounded-lg p-8 w-full"></div>
-        </CardFooter>
+        <CardFooter className="flex flex-col gap-1"></CardFooter>
       </Card>
     </div>
   );
@@ -94,5 +76,105 @@ function ProgressBar() {
   }, []);
   return <Progress value={progress} className="" />;
 }
+
+async function textAnalyse(text: string): Promise<TextAnalysisRes> {
+  const frequencies: Record<string, number> = {};
+  const positions: Record<string, number[]> = {};
+  const stats = {} as {
+    word_count: number;
+    char_count: number;
+  };
+
+  const re = /\w+/g;
+  let res = text.match(re);
+
+  res?.map((w, index) => {
+    const word = w.toLowerCase();
+    const v = positions[word] ?? [];
+    v.push(index);
+    positions[word] = v;
+    frequencies[word] = (frequencies[word] ?? 0) + 1;
+  });
+  stats.word_count = res!.length;
+  stats.char_count = text.length;
+
+  return {
+    word_pos: positions,
+    word_freqs: frequencies,
+    stats,
+  };
+}
+
+export function UplodedState() {
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center bg-gray-100 rounded-lg p-4 w-full">
+      {/* header */}
+      <div className="flex w-full justify-between items-start mb-2 border-0 border-neutral-500">
+        <div className="flex gap-2 items-center">
+          {/* Icon */}
+          <File size={24} />
+          <p className="font-semibold text-sm">Filename.ext</p>
+        </div>
+        {/* close Btn */}
+        <div className="p-1 rounded-full hover:border-1 hover:border-gray-500 cursor-pointer transition-all duration-100 ease-in-out">
+          <EllipsisVertical size={16} />
+        </div>
+      </div>
+      <div className="flex gap-5 justify-between w-full items-start">
+        <p className="text-xs text-gray-500 flex-grow flex-2/3">
+          Click analyse to begin processing your file or download it using
+          the&nbsp;
+          <span className="font-bold bg-gray-400 p-0 text-white py-[1px] rounded-xs">
+            ︙
+          </span>
+          &nbsp;icon above.
+        </p>
+        <div className="flex flex-1/3 flex-col items-end justify-start">
+          <Button
+            variant={"default"}
+            className="cursor-pointer shadow-sm shadow-gray-200 !bg-white !text-foreground hover:scale-105"
+          >
+            Analyse
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function UploadingState() {
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center bg-gray-100 rounded-lg p-4 w-full">
+      {/* header */}
+      <div className="flex w-full justify-between items-start mb-1 border-0 border-neutral-500">
+        <div className="flex gap-3 items-center">
+          {/* Icon */}
+          <div className="size-10 rounded-md bg-white shadow-md shadow-gray-200 flex flex-col items-center justify-center p-2">
+            <File />
+          </div>
+          {/* file info */}
+          <div className="flex flex-col justify-center">
+            <p className="font-medium text-sm">Filename.ext</p>
+            <p className="font-normal text-xs text-gray-400">3 MB</p>
+          </div>
+        </div>
+        {/* close Btn */}
+        <div className="p-1 rounded-full hover:border-1 hover:border-gray-500 cursor-pointer transition-all duration-100 ease-in-out">
+          <X size={16} />
+        </div>
+      </div>
+      <ProgressBar />
+    </div>
+  );
+}
+
+export type TextAnalysisRes = {
+  word_pos: Record<string, number[]>;
+  word_freqs: Record<string, number>;
+  stats: {
+    word_count: number;
+    char_count: number;
+  };
+};
 
 export default App;
---
+++ texios/client/src/components/FileSelect.tsx
@@ -0,0 +1,138 @@
+import { CloudUpload, File } from "lucide-react";
+import { Button } from "./ui/button";
+import { useEffect, useRef, useState } from "react";
+import { Spinner } from "./ui/spinner";
+
+const sizes: Record<string, SizeUnit> = {
+  1000: "KB",
+  1000000: "MB",
+  1000000000: "GB",
+};
+type SizeUnit = "GB" | "KB" | "MB";
+
+export function FileSelect({
+  analyseText,
+}: {
+  analyseText: (t: string) => void;
+}) {
+  const [file, setFile] = useState<File | null>(null);
+  const [fileText, setFileText] = useState<string | null>(null);
+  const [error, setError] = useState<{ type: string; message: string } | null>(
+    null
+  );
+  const [fileSize, setFileSize] = useState<{
+    value: number;
+    unit: SizeUnit;
+  } | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const fileTypes = ".txt,.md,.json,.csv,.js,.py,.ts,.sql";
+
+  useEffect(() => {
+    if (file) {
+      let div = 1000;
+      while (file.size / div > 1000) {
+        div *= 1000;
+      }
+      let size = Math.round((file.size / div) * 100) / 100;
+      setFileSize({ value: size, unit: sizes[div] });
+
+      const fileReader = new FileReader();
+      console.log("file", file);
+      // get text content
+      const supportedTypes = fileTypes.split(",");
+      if (
+        file.type.startsWith("text") ||
+        (!file.type && supportedTypes.some((v) => file.name.endsWith(v)))
+      ) {
+        fileReader.onloadend = (e) => setFileText(e.target?.result as string);
+        fileReader.readAsText(file);
+      } else {
+        setFileText(null);
+        setError({ type: "unsupported", message: "File Not Supported" });
+      }
+    }
+  }, [file]);
+
+  useEffect(() => {
+    console.log("file Text:", fileText);
+    analyseText("This is a test");
+  }, [fileText]);
+
+  useEffect(() => {
+    console.log("error:", error);
+  }, [error?.type, error?.message]);
+
+  return (
+    <div className="flex flex-col gap-3 justify-center items-center border-1 border-dashed border-gray-300 rounded-lg p-6 flex-grow flex-3/4">
+      {!file && <Empty />}
+      {/* File Preview */}
+      {file && (
+        <div className="flex flex-col gap-2 justify-center items-center bg-gray-100/70 rounded-lg py-5 px-8 min-w-3/5 max-w-full">
+          <div className="size-10 rounded-md bg-white shadow-md shadow-gray-200 flex flex-col items-center justify-center p-2">
+            <File />
+          </div>
+          <h4>{file.name}</h4>
+          {fileSize && (
+            <p>
+              {fileSize.value} {fileSize.unit}
+            </p>
+          )}
+        </div>
+      )}
+      <input
+        ref={inputRef}
+        type="file"
+        className="border-1 border-neutral-100 p-5 hidden"
+        accept={fileTypes}
+        onChange={(e) => {
+          let files = (e.target as HTMLInputElement).files;
+          if (files && files.length > 0) {
+            setFile(files[0]);
+          } else {
+            setFile(null);
+          }
+        }}
+      />
+      <div className="flex gap-5 justify-center items-center">
+        <Button
+          variant="outline"
+          size="sm"
+          className="mt-3 cursor-pointer"
+          onClick={() => {
+            if (inputRef.current) {
+              inputRef.current.click();
+            }
+          }}
+        >
+          Select File
+        </Button>
+        {file && (
+          <Button
+            size="sm"
+            className="mt-3 cursor-pointer flex items-center gap-1"
+            onClick={() => {
+              console.log("Uploading file...");
+            }}
+          >
+            <Spinner className="size-4" />
+            Analyse
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Empty() {
+  return (
+    <div className="flex flex-col gap-3 justify-center items-center">
+      <CloudUpload className="size-12 text-neutral-300" />
+      <div className="flex flex-col gap-1 justify-center items-center">
+        <p>Choose file or drag and drop it here</p>
+        <p className="text-gray-400 text-xs">
+          TXT, PDF, MD, DOC DOCX up to&nbsp;<span>50mb</span>
+        </p>
+      </div>
+    </div>
+  );
+}
