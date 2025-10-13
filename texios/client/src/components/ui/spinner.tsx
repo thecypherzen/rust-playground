471d1794534d5b0527ecc5b9c2fb53b45a209d3b
William Inyam <sw.inyam@outlook.com>
2025-10-13 04:03:32 +0100

    :up: update: add optional wrapper around spinner

--- texios/client/src/components/ui/spinner.tsx
+++ texios/client/src/components/ui/spinner.tsx
@@ -1,16 +1,32 @@
-import { Loader2Icon } from "lucide-react"
+import { Loader2Icon } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
-  return (
+function Spinner({
+  className,
+  wrap,
+  ...props
+}: React.ComponentProps<"svg"> & { wrap?: "screen" | "parent" }) {
+  const e = (
     <Loader2Icon
       role="status"
       aria-label="Loading"
       className={cn("size-4 animate-spin", className)}
       {...props}
     />
-  )
+  );
+  return wrap ? (
+    <div
+      className={cn(
+        "w-full flex flex-col items-center justify-center",
+        wrap === "screen" ? "h-screen" : "h-full"
+      )}
+    >
+      {e}
+    </div>
+  ) : (
+    e
+  );
 }
 
-export { Spinner }
+export { Spinner };
